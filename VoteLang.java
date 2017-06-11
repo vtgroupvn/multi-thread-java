dcb84608aa8a1d5880efd0f6cb4e5956dde3d0ed
VTGroup <infor.vtgroup@gmail.com>
2017-06-10 21:15:55 -0700

    Java multi-thread, Votes java examples

---
+++ VoteLang.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package votes;
+class LangItem{
+    public String key;
+    public String Value;
+    LangItem(String key, String Value){
+        this.key = key;
+        this.Value = Value;
+    }
+}
+/**
+ *
+ * @author admin
+ */
+public class VoteLang {
+    /**
+     * Array LangItem
+     */
+    private LangItem[] _langs;
+    /**
+     * Import languages
+     * 
+     * @param _langs 
+     */
+    public void _importLang(LangItem[] _langs)
+    {
+        this._langs = new LangItem[_langs.length];
+        for(int i = 0; i < _langs.length; i++){
+            this._langs[i] = _langs[i];
+        }
+    }
+    /**
+     * Return lang String
+     * 
+     * @param key
+     * @return 
+     */
+    public String __l(String key)
+    {
+        int found = 0;
+        boolean hasLang = false;
+        for(int i = 0; i < this._langs.length; i++){
+            if (this._langs[i].key == key){
+                found = i;
+                hasLang = true;
+                break;
+            }
+        }
+        if (hasLang){
+            return this._langs[found].Value;
+        }
+        return key;
+    }
+    /**
+     * TODO yourself
+     * 
+     * @param key
+     * @param _args
+     * @return 
+     */
+    public String __p(String key, String[] _args)
+    {
+        //TODO: print_r from _args
+        return key;
+    }
+}
